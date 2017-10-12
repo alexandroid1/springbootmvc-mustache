896e94f263f5421a884989a3b9a4e1e571b9e22d
Alexandr <avpavlov108@gmail.com>
2017-10-12 17:16:03 +0300

    Initial commit for mustache proj

---
+++ src/main/java/com/springbootmvcmustache/springbootmvcmustache/SpringbootmvcMustacheApplication.java
@@ -0,0 +1,12 @@
+package com.springbootmvcmustache.springbootmvcmustache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmvcMustacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootmvcMustacheApplication.class, args);
+	}
+}
---
+++ src/test/java/com/springbootmvcmustache/springbootmvcmustache/SpringbootmvcMustacheApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springbootmvcmustache.springbootmvcmustache;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootmvcMustacheApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
