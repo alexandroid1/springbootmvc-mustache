411c56a2376a0dfbf38d92ad821e53f5c7a06833
Alexandr <avpavlov108@gmail.com>
2017-10-12 18:22:53 +0300

    mustache method

--- src/main/java/com/springbootmvcmustache/springbootmvcmustache/SpringbootmvcMustacheApplication.java
+++ src/main/java/com/springbootmvcmustache/springbootmvcmustache/SpringbootmvcMustacheApplication.java
@@ -2,9 +2,19 @@ package com.springbootmvcmustache.springbootmvcmustache;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@Controller
 public class SpringbootmvcMustacheApplication {
+
+	@RequestMapping("/")
+	public String mustache(Model model){
+		model.addAttribute("company", "OReily");
+		return "template";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootmvcMustacheApplication.class, args);
